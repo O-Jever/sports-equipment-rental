636dce595c585d91115e7963d2f1c10ab0fb2e5b
Османова Джевер <osmanova@simferopol.softlab.ru>
2021-10-07 17:47:22 +0300

    feat(contacts): Создана старница с контактами

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { ContactsPage } from '../pages/contacts/contacts';
 
 @NgModule({
-  declarations: [MyApp, ListPage, ItemDetailsPage, FilterPage],
+  declarations: [MyApp, ListPage, ItemDetailsPage, FilterPage, ContactsPage],
   imports: [BrowserModule, IonicModule.forRoot(MyApp), ReactiveFormsModule],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp, ListPage, ItemDetailsPage, FilterPage],
+  entryComponents: [MyApp, ListPage, ItemDetailsPage, FilterPage, ContactsPage],
   providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }],
 })
 export class AppModule {}
